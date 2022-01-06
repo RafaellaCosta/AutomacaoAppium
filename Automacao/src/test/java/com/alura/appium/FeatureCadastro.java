0288c0b66b9a78ee88b4badd7b5cae71b3dd192b
Rafaella_Costa <rafaellamcp@gmail.com>
2022-01-06 18:40:10 -0300

    projeto de automacao com a alura

---
+++ Automacao/src/test/java/com/alura/appium/FeatureCadastro.java
@@ -0,0 +1,55 @@
+package com.alura.appium;
+
+import static org.junit.Assert.assertTrue;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.NoSuchElementException;
+
+import java.net.MalformedURLException;
+
+
+public class FeatureCadastro
+{
+    @Test
+    public void nao_consigo_cadastrar_usuario_com_senhas_que_nao_conferem() {
+        AppiumDriver driver = AppiumDriverConfig.Instance().driver;
+
+        MobileElement botaoCadastro = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+        botaoCadastro.click();
+        MobileElement campoNome = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/input_nome");
+        MobileElement campoSenha = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/input_senha");
+        MobileElement campoConfirmarSenha = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/input_confirmar_senha");
+        campoNome.setValue("Rafa");
+        campoSenha.setValue("123");
+        campoConfirmarSenha.setValue("456");
+
+        MobileElement botaoConfirmarCadastro = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar");
+        botaoConfirmarCadastro.click();
+        MobileElement erro = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/erro_cadastro");
+
+        Assert.assertEquals("Senhas não conferem", erro.getText());
+        driver.navigate().back();
+    }
+
+    @Test
+    public void posso_cadastrar_usuario_com_senhas_que_conferem() throws NoSuchElementException {
+        AppiumDriver driver = AppiumDriverConfig.Instance().driver;
+        MobileElement botaoCadastro = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario");
+        botaoCadastro.click();
+
+        MobileElement campoNome = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/input_nome");
+        MobileElement campoSenha = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/input_senha");
+        MobileElement campoConfirmarSenha = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/input_confirmar_senha");
+        campoNome.setValue("Rafa");
+        campoSenha.setValue("123");
+        campoConfirmarSenha.setValue("123");
+
+        MobileElement botaoConfirmarCadastro = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar");
+        botaoConfirmarCadastro.click();
+
+        MobileElement botaoLogar = (MobileElement) driver.findElementById("br.com.alura.aluraesporte:id/login_botao_logar");
+    }
+}
